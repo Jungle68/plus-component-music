abf76d21f8fdf953ee06346b7078e38795626386
白石 <414606094@qq.com>
2017-03-22 10:04:16 +0800

    歌曲详情加入赞字段

--- src/Controllers/MusicController.php
+++ src/Controllers/MusicController.php
@@ -3,28 +3,33 @@
 namespace Zhiyi\Component\ZhiyiPlus\PlusComponentMusic\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Zhiyi\Plus\Http\Controllers\Controller;
 use Zhiyi\Component\ZhiyiPlus\PlusComponentMusic\Models\Music;
+use Zhiyi\Component\ZhiyiPlus\PlusComponentMusic\Models\MusicDigg;
 use Zhiyi\Component\ZhiyiPlus\PlusComponentMusic\Models\MusicSpecial;
 use Zhiyi\Component\ZhiyiPlus\PlusComponentMusic\Models\MusicSpecialLink;
 
 class MusicController extends Controller
 {
-	public function getMusicInfo(Request $request, $music_id)
-	{
-		$musicInfo = Music::where('id', $music_id)->with(['singer' => function ($query) {
+    public function getMusicInfo(Request $request, $music_id)
+    {
+        $uid = Auth::guard('api')->user()->id ?? 0;
+        $musicInfo = Music::where('id', $music_id)->with(['singer' => function ($query) {
             $query->with('cover');
         }])->first();
-		if (!$musicInfo) {
+        if (!$musicInfo) {
            return response()->json([
                 'status' => false,
                 'code' => 8002,
                 'message' => '歌曲不存在或已被删除'
             ])->setStatusCode(404); 
-		}
+        }
 
-		Music::where('id', $music_id)->increment('taste_count'); // 歌曲增加播放数量
-		MusicSpecial::whereIn('id', MusicSpecialLink::where('music_id', $music_id)->pluck('special_id'))->increment('taste_count'); // 相应专辑增加播放数量
+        $musicInfo->isdiggmusic = MusicDigg::where(['user_id' => $uid, 'music_id' => $music_id])->first() ? 1 : 0;
+
+        Music::where('id', $music_id)->increment('taste_count'); // 歌曲增加播放数量
+        MusicSpecial::whereIn('id', MusicSpecialLink::where('music_id', $music_id)->pluck('special_id'))->increment('taste_count'); // 相应专辑增加播放数量
 
         return response()->json([
             'status'  => true,
@@ -32,5 +37,5 @@ class MusicController extends Controller
             'message' => '获取成功',
             'data' => $musicInfo
         ])->setStatusCode(200);
-	}
+    }
 }
