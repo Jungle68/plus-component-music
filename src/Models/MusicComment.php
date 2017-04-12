d3c4ca35df4d37011c22094a6d72c73417f24a59
白石 <414606094@qq.com>
2017-04-12 15:32:06 +0800

    增加歌曲 评论关联

--- src/Models/MusicComment.php
+++ src/Models/MusicComment.php
@@ -11,7 +11,23 @@ class MusicComment extends Model
 {
     protected $table = 'music_comments';
 
-    
+    /**
+     * 单条评论属于一条音乐
+     * @return [type] [description]
+     */
+    public function music()
+    {
+        return $this->belongsTo(Music::class, 'music_id', 'id');
+    }
+
+    /**
+     * 单条评论属于一条专辑
+     * @return [type] [description]
+     */
+    public function special()
+    {
+        return $this->belongsTo(MusicSpecial::class, 'special_id', 'id');
+    }
     /**
      * 覆盖删除方法  同步到tsplus评论表
      * 
@@ -24,6 +40,7 @@ class MusicComment extends Model
         $comment = [
             'component' => 'music',
             'comment_table' => 'music_comments',
+            'comment_content' => $this->comment_content,
             'user_id' => $this->user_id,
             'to_user_id' => $this->to_user_id,
             'reply_to_user_id' => $this->reply_to_user_id,            
@@ -32,9 +49,13 @@ class MusicComment extends Model
         if ($this->music_id > 0) {
             $comment['source_table'] = 'musics';
             $comment['source_id'] = $this->music_id;
+            $comment['source_cover'] = $this->music->singer->cover;
+            $comment['source_content'] = $this->music->title;
         } else {
             $comment['source_table'] = 'music_specials';
             $comment['source_id'] = $this->special_id;
+            $comment['source_cover'] = $this->special->storage;
+            $comment['source_content'] = $this->special->title;
         }
 
         DB::transaction(function () use ($comment) {
