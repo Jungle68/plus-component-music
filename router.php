7ca29bc8ddaf470c01f6b2fb242f870b911b6700
白石 <414606094@qq.com>
2017-07-19 14:11:42 +0800

    slimkit/thinksns-plus#91 增加v2路由文件

--- router.php
+++ router.php
@@ -15,3 +15,8 @@ Route::prefix('api/v1')
     ->middleware('api')
     ->namespace('Zhiyi\\Component\\ZhiyiPlus\\PlusComponentMusic\\Controllers')
     ->group(component_base_path('/routes/api.php'));
+
+Route::prefix('api/v2')
+    ->middleware('api')
+    ->namespace('Zhiyi\\Component\\ZhiyiPlus\\PlusComponentMusic\\Controllers\\V2')
+    ->group(component_base_path('/routes/api_v2.php'));
