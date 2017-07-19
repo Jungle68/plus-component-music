2a6f0456c49d983aac44bde18bfd87576962727e
白石 <414606094@qq.com>
2017-07-19 11:49:46 +0800

    增加 关注模型关联

--- src/Models/MusicSpecial.php
+++ src/Models/MusicSpecial.php
@@ -4,8 +4,9 @@ namespace Zhiyi\Component\ZhiyiPlus\PlusComponentMusic\Models;
 
 use Zhiyi\Plus\Models\PaidNode;
 use Zhiyi\Plus\Models\FileWith;
-use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class MusicSpecial extends Model
 {
@@ -22,6 +23,16 @@ class MusicSpecial extends Model
     }
 
     /**
+     * 专辑收藏记录.
+     *
+     * @author bs<414606094@qq.com>
+     */
+    public function collections()
+    {
+    	return $this->hasMany(MusicCollection::class, 'special_id', 'id');
+    }
+
+    /**
      * 专辑付费节点
      * 
      * @author bs<414606094@qq.com>
@@ -29,5 +40,25 @@ class MusicSpecial extends Model
     public function paidNode()
     {
         return $this->hasOne(PaidNode::class, 'raw', 'id')->where('channel', 'music');
+    }
+
+    /**
+     * 验证某个用户是否收藏了某个专辑  
+     *
+     * @author bs<414606094@qq.com>
+     * @param  [int]  $user
+     * @return boolean
+     */
+    public function hasCollected(int $user): bool
+    {
+        $cacheKey = sprintf('music-special-collected:%s,%s', $this->id, $user);
+        if (Cache::has($cacheKey)) {
+            return Cache::get($cacheKey);
+        }
+
+        $status = $this->collections()->newPivotStatementForId($user)->first() !== null;
+        Cache::forever($cacheKey, $status);
+
+        return $status;
     }
 }
